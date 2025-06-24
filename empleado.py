939bf7fc702cce25d9afacfef2488440786e1b41
jeanCOA <jeancarloospinaalzate542@gmail.com>
2025-06-24 11:59:27 -0500

    error ventas negativas resuelto

--- empleado.py
+++ empleado.py
@@ -11,6 +11,7 @@ def menu_principal():
         print("************** MENU PRINCIPAL **************")
         
         print("""
+            
             1. MENU CLIENTES
             2. MENU PRODUCTOS
             3. VENDER
@@ -27,13 +28,12 @@ def menu_principal():
             case 1:menu_clientes()
             case 2:menu_productos()
             case 3:vender()
-            # case 4:menu_factura()
             case 0: 
                 print("*************HASTA LA VISTA BBY***************")
                 activo = False
             case _: print("opc incorrecta")
 
-   
+
 
 # -----------------------------------CRUD PRODUCTOS----------------------------------------------------------------------
 def menu_productos():
@@ -60,15 +60,28 @@ def menu_productos():
                 activo=False
             case _:print("Opc incorrecta")
 
+
+
 def agregar_producto():
-    id=input("INGRESE EL ID DEL PRODUCTO: ")
-    tipo= input("INGRESE EL TIPO DEL PRODUCTO(BOLIS,PALETA,HELADO CUADRADO,HELADO REDONDO): ")
-    sabor=input("INGRESE EL SABOR(NARANJA,PIÑA,MORA,TRICOLOR,LIMON,ETC): ")
-    stock= input("CANTIDAD DE PRODUCTO: ")
-    precio=input("INGRESE PRECIO DEL PRODUCTO: ")
-    with open("productos.csv","a") as file:
+    id = input("INGRESE EL ID DEL PRODUCTO: ")
+    try:
+        with open("productos.csv", "r") as file:
+            for linea in file:
+                datos = linea.strip().split(";")
+                if datos[0] == id:
+                    print(f"ERROR: El ID DEL PRODUCTO ES EL  '{id}' YA EXISTE.")
+                    input("\nPresiona Enter para continuar...")
+                    return
+    except FileNotFoundError:
+        pass 
+    tipo = input("INGRESE EL TIPO DEL PRODUCTO (BOLIS, PALETA, HELADO CUADRADO, HELADO REDONDO): ")
+    sabor = input("INGRESE EL SABOR (NARANJA, PIÑA, MORA, TRICOLOR, LIMÓN, ETC): ")
+    stock = input("CANTIDAD DE PRODUCTO: ")
+    precio = input("INGRESE PRECIO DEL PRODUCTO: ")
+    with open("productos.csv", "a") as file:
         file.write(f"{id};{tipo};{sabor};{stock};{precio}\n")
-        print('PRODUCTO AGREGADO CON EXITO')
+        print('PRODUCTO AGREGADO CON ÉXITO')
+# ____________________________________________________________________________________________________
 
 
 def listar_productos():
@@ -95,8 +108,8 @@ def actualizar_producto():
             lista_fila = fila.split(';')
             if lista_fila[0] == id:
                 id= input('INGRESE LA NUEVA ID: ')
-                tipo= input('INGRESE EL NUEVO TIPO: ')
-                sabor= input('INGRESE EL NUEVO SABOR: ')
+                tipo= input('INGRESE EL NUEVO TIPO: ').lower()
+                sabor= input('INGRESE EL NUEVO SABOR: ').lower()
                 stock= input('INGRESE LA NUEVA CANTIDAD: ')
                 precio= input('INGRESE EL NUEVO PRECIO: ')
                 nueva_fila = id + ';' + tipo +';'+ sabor +';'+ stock +';'+precio + '\n'
@@ -159,7 +172,7 @@ def menu_clientes():
 
 lista_cliente = []
 def agregar_clientes():
-    
+    print("***************AGREGAR CLIENTE*******************")
     id = input("INGRESE EL CC DEL CLIENTE: ")
     cliente_existe = False
     try:
@@ -234,7 +247,7 @@ def actualizar_clientes():
             lista_fila = fila.split(';')
             if lista_fila[0] == id:
                 id= input('INGRESE LA NUEVA ID: ')
-                nombre= input('INGRESE EL NUEVO NOMBRE: ')
+                nombre= input('INGRESE EL NUEVO NOMBRE: ').lower()
                 
                 nueva_fila = id + ';' + nombre + '\n'
                 lista_copia.append(nueva_fila)
@@ -349,6 +362,7 @@ def vender():
                     precio = int(partes[4])
                     if cantidad > stock:
                         print("No hay suficiente stock.")
+                        input('presione enter para continuar')
                         return
                     subtotal = cantidad * precio
                     carrito.append([prod_id, tipo, sabor, cantidad, precio, subtotal])
@@ -398,20 +412,6 @@ menu_principal()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # def menu_facturas():
     
 #     activo=True
@@ -436,3 +436,6 @@ menu_principal()
 #                 print("*************HASTA LA VISTA BBY***************")
 #                 activo=False
 #             case _:print("OPC INCORRECTA")
+
+
+#hola
